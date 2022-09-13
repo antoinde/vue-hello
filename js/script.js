668afd6be0c4fa8a5f3a5951465f86ebf1073748
antoinde <antonello.indelicato98@gmail.com>
2022-09-13 09:40:17 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // Antonello's code. Git-Hub: antoinde
 
 console.log("JavaScript is working properly! =) Let's code!");
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Ciao utente!'
+    }
+});
